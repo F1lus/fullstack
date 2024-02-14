903ef3024e1d75bf358e655feaef11ab83c62c69
F1lus <filimon.mark@gmail.com>
2024-02-14 11:42:55 +0100

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+/**
+ * Default Route (/)
+ * 
+ * Automatically rendered when the root of the page
+ * is visited
+ * 
+ * @returns JSX
+ */
+export default function Home() {
+  return (
+    <main></main>
+  );
+}
