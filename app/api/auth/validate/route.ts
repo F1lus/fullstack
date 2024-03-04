81f330f999b46921b5895411495b62d693fafe3c
F1lus <filimon.mark@gmail.com>
2024-03-04 11:05:08 +0100

    Modify authentication: Login now returns the token in every successful case, and the token should be stored in an Authentication header rather than in a cookie

--- app/api/auth/validate/route.ts
+++ app/api/auth/validate/route.ts
@@ -1,9 +1,9 @@
 import { JSONResponse } from "@/app/lib/api/JSONResponse"
-import { isCookieSessionValid } from "@/app/lib/auth/SessionHandler"
+import { isAuthorizationHeaderValid } from "@/app/lib/auth/SessionHandler"
 
 export const dynamic = 'force-dynamic'
 
-export async function GET(request: Request) {
-    const isLoggedIn = await isCookieSessionValid()
-    return JSONResponse.status(200).send({ isLoggedIn })
+export async function GET(_: Request) {
+    const isLoggedIn = await isAuthorizationHeaderValid()
+    return JSONResponse.status(isLoggedIn ? 200 : 401).send()
 }
--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,55 @@
 'use server'
 
 import {NextRequest, NextResponse} from "next/server";
-import {Transfer} from "./app/lib/api/transfer";
 
 export async function middleware(request: NextRequest) {
-    const {isLoggedIn} = await (await Transfer('/auth/validate')).get()
     const url = request.nextUrl.pathname
     const origin = request.nextUrl.origin
+    const authorization = request.headers.get('Authorization')
 
     console.log('Middleware called for:', url)
 
-    if (url.startsWith('/_next') || url.startsWith('/images')) {
-        return NextResponse.next()
+    try {
+        const response = await fetch(`${origin}/api/auth/validate`, {
+            headers: {
+                'Authorization': `${authorization}`
+            }
+        })
+        const isLoggedIn = response.status === 200
+
+
+        if (url.startsWith('/_next') || url.startsWith('/images')) {
+            return NextResponse.next()
+        }
+
+        if (url.startsWith('/api')) {
+            //#region API
+            if (isLoggedIn && url.startsWith('/api/auth')) {
+                const redirectUrl = new URL('/api', origin)
+                return NextResponse.redirect(redirectUrl)
+            }
+
+            if (!isLoggedIn && !url.startsWith('/api/auth')) {
+                const redirectUrl = new URL('/api/auth/login', origin)
+                console.log(redirectUrl.href)
+                return NextResponse.redirect(redirectUrl)
+            }
+            //#endregion
+        } else {
+            //#region Frontend
+            if (isLoggedIn && url.startsWith('/auth')) {
+                const redirectUrl = new URL('/', origin)
+                return NextResponse.redirect(redirectUrl)
+            }
+
+            if (!isLoggedIn && !url.startsWith('/auth')) {
+                const redirectUrl = new URL('/auth/login', origin)
+                return NextResponse.redirect(redirectUrl)
+            }
+            //#endregion
+        }
+    } catch (err){
+        console.error('Middleware:', 'Authentication failed!', err)
+        return NextResponse.error()
     }
-
-    if (url.startsWith('/api')) {
-        //#region API
-        if (isLoggedIn && url.startsWith('/api/auth')) {
-            const redirectUrl = new URL('/api', origin)
-            return NextResponse.redirect(redirectUrl)
-        }
-
-        if (!isLoggedIn && !url.startsWith('/api/auth')) {
-            const redirectUrl = new URL('/api/auth/login', origin)
-            console.log(redirectUrl.href)
-            return NextResponse.redirect(redirectUrl)
-        }
-        //#endregion
-    } else {
-        //#region Frontend
-        if (isLoggedIn && url.startsWith('/auth')) {
-            const redirectUrl = new URL('/', origin)
-            return NextResponse.redirect(redirectUrl)
-        }
-
-        if (!isLoggedIn && !url.startsWith('/auth')) {
-            const redirectUrl = new URL('/auth/login', origin)
-            console.log(redirectUrl.href)
-            return NextResponse.redirect(redirectUrl)
-        }
-        //#endregion
-    }
-
 }
